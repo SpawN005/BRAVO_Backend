fa10bf9c01a74d768940f2c85deb7c3b5bd14166
Jihed Larayedh <larajihade@gmail.com>
2024-02-24 19:33:27 +0100

    user fixes

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -18,7 +18,7 @@ exports.insert = (req, res) => {
         .update(req.body.password)
         .digest("base64");
       req.body.password = salt + "$" + hash;
-      // req.body.permissionLevel = 1;
+      // req.body.permissionLevel= 1;
       UserModel.createUser({ userIdentity: req.body }).then((result) => {
         if (result != undefined) {
           result = result.toJSON();
