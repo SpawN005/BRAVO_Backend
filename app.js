510082bb57bc5aa2cb8bf227e70ce0118c7e5cb3
SpawN 005 <jasserbouzidi22@gmail.com>
2024-03-14 20:46:28 +0100

    tournament get test

--- app.js
+++ app.js
@@ -1,36 +1,29 @@
 // BASE SETUP
 // ==============================================
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-
-
 var cors = require("cors");
-
-
 
 var port = process.env.PORT || 3001;
 const connectDB = require("./src/dbConfig/mongoose");
 var bodyParser = require("body-parser");
 const usersRouter = require("./src/routes/usersRoutes/users.router");
-const tournamentRouter = require('./src/routes/tournamentRoutes/tournament.router'); 
+const tournamentRouter = require("./src/routes/tournamentRoutes/tournament.router");
 
 const swaggerDoc = require("./src/docs/swaggerDoc");
 const stadiumRoutes = require("./src/routes/stadiumRoutes/stadiumRoutes");
 const matchRoutes = require("./src/routes/matchRoutes/matchRoutes");
-const playerRoute = require('./src/routes/playerRoutes/player')
-const teamRoute = require('./src/routes/teamRoutes/teams')
-const path = require('path');
-const matchStatsRoutes = require ("./src/routes/matchRoutes/matchStatsRoutes");
+const playerRoute = require("./src/routes/playerRoutes/player");
+const teamRoute = require("./src/routes/teamRoutes/teams");
+const path = require("path");
+const matchStatsRoutes = require("./src/routes/matchRoutes/matchStatsRoutes");
 // const server = http.createServer(app);
 // const io = require("socket.io")(server);
-
-
-
 
 connectDB();
 // ==============================================
@@ -42,10 +35,9 @@ tournamentRouter(app);
 
 swaggerDoc(app);
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
 });
-
 
 // ==============================================
 // START THE SERVER
@@ -56,17 +48,18 @@ app.use("/stadiums", stadiumRoutes);
 // app.use("/match", matchRoutes);
 
 app.use("/matches", matchRoutes);
-app.use('/player',playerRoute)
-app.use('/team',teamRoute)
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/player", playerRoute);
+app.use("/team", teamRoute);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Route pour servir les logos
-app.get('/logo/:filename', (req, res) => {
+app.get("/logo/:filename", (req, res) => {
   const { filename } = req.params;
-  res.sendFile(path.join(__dirname, 'uploads', filename));
+  res.sendFile(path.join(__dirname, "uploads", filename));
 });
 //matchmaking(app);
 // app.use("/match", matchRoutes);
-app.use('/match-stats', matchStatsRoutes);
+app.use("/match-stats", matchStatsRoutes);
 // app.use("/matchStat", matchStatsRoutes()); // Pass the io instance to matchStatsRoutes
 
+module.exports = app;
---
+++ src/tests/tournament.test.js
@@ -0,0 +1,14 @@
+const app = require("../../app"); // Link to your server file
+const supertest = require("supertest");
+const request = supertest(app);
+const Tournament = require("../models/tournament.js");
+
+describe("test tournament routes", () => {
+  describe("GET /tournaments", () => {
+    it("should get all tournaments successfully", async () => {
+      const response = await request.get("/tournaments");
+      expect(response.status).toBe(200);
+      expect(response.body).toBeDefined();
+    });
+  });
+});
